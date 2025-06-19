d804eccc4c02ebc00ba3ce4c491d4b383c37e60a
Run Lin <no101shark@gmail.com>
2025-06-19 16:15:52 +0800

    fix

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	ticker := time.NewTicker(3 * time.Second)
+	defer ticker.Stop()
+
+	count := 0
+	for range ticker.C {
+		count++
+		fmt.Printf("tick #%d\n", count)
+
+	}
+
+	fmt.Println("loop ended")
+
 }
